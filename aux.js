9ecb933231712fd83d47c783886e75acb0a05dda
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2019-02-08 19:03:41 +0100

    Comienzo del proyecto -DONE

--- aux.js
+++ aux.js
@@ -1,5 +1,17 @@
-function comenzar(){
+function presentacion(){
     setTimeout(function(){
-        $(document.body).append(`<div id='divComienzo'></div>`);
+        var div = $(`<div id='divComienzo' style='display: none'></div>`);
+        $(div).append(`<h1 class="titulo">Bienvenido al gestor de contenido de Gallego</h1>`);
+        $(div).append(`<h1 class="titulo1">Proyecto DIW</h1>`);
+        $(div).append(`<button> Comenzar </button>`);
+        $(document.body).append(div);
+        $(div).fadeIn(1000);
+        $('button').click(function(){
+            $('div').remove();
+        });
     }, 3000);
+}
+
+function comenzar(){
+    
 }
